9b2f78efc3429d251f303467a2d9dcbfd16db0e8
Aleksandr Alekseev <alekseev.a@atbgroup.ru>
2024-05-22 11:14:52 +0300

    init

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,21 @@
 <template>
   <v-app>
+    <v-app-bar :elevation="2">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
+
     <v-main>
-      <router-view />
+      <router-view/>
     </v-main>
 
-    <AppFooter />
+    <AppFooter/>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
