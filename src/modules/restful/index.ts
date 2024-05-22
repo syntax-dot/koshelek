4ac6f3ced0c9ad5e60208e1cdf9c872dc4f4b61d
Aleksandr Alekseev <alekseev.a@atbgroup.ru>
2024-05-22 15:50:50 +0300

    init

--- src/modules/restful/index.ts
+++ src/modules/restful/index.ts
@@ -1,12 +1,11 @@
 import {Spot} from '@binance/connector-typescript';
+import {Axios} from "axios";
 
 const API_KEY = '';
 const API_SECRET = '';
 const BASE_URL = 'https://testnet.binance.vision';
 
 const client = new Spot(API_KEY, API_SECRET, {baseURL: BASE_URL});
-client.exchangeInformation().then((res) => {
-  console.log(res);
-}).catch(err => {
-  console.log(err)
-});
+export const api = new Axios({
+  baseURL: BASE_URL
+})
