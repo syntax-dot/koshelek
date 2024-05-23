7be8d0b62102ae882471f73475803a6eacaab8f0
Aleksandr Alekseev <alekseev.a@atbgroup.ru>
2024-05-23 23:38:39 +0300

    wip

--- src/pages/OrderBookPage.vue
+++ src/pages/OrderBookPage.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <v-select
-      v-model="selectedLimit"
-      variant="outlined"
-      label="Items limit"
-      :items="limitsOptions"
-    />
-    <v-select
-      v-model="selectedPriceDifference"
-      variant="outlined"
-      clearable
-      label="Price difference"
-      :items="priceDifferenceOptions"
-    />
+    <div class="order_params">
+      <v-select
+        v-model="selectedLimit"
+        variant="outlined"
+        label="Items limit"
+        :items="limitsOptions"
+      />
+      <v-select
+        v-model="selectedPriceDifference"
+        variant="outlined"
+        clearable
+        label="Price difference"
+        :items="priceDifferenceOptions"
+      />
+    </div>
     <div class="order_wrapper" v-if="askOrders">
       <OrderTable :columns="columns" :orders="askOrders" color="#CA374E"/>
       <OrderTable :columns="columns" :orders="bidOrders" color="#2EBD85"/>
@@ -28,7 +30,7 @@ import {useSelectedPair} from "../hooks/use-selected-pair";
 
 const columns = ['Price', 'Quantity', 'Total']
 const limitsOptions = [100, 500, 1000]
-const priceDifferenceOptions = [0.01, 0.1, 1, 10, 100]
+const priceDifferenceOptions = [1, 10, 100]
 
 const selectedLimit = ref(100)
 const selectedPriceDifference = ref<number | null>(null)
@@ -45,10 +47,16 @@ const bidOrders = useGroupedItems(orderBook.bidOrders, selectedPriceDifference)
 </script>
 
 <style scoped lang="sass">
-.order_wrapper
-  min-height: 400px
-  display: grid
-  grid-template-columns: 1fr 1fr
-  gap: 16px
+.order
+  &_wrapper
+    min-height: 400px
+    display: grid
+    grid-template-columns: 1fr 1fr
+    gap: 16px
+
+  &_params
+    display: grid
+    grid-template-columns: 1fr 1fr
+    gap: 16px
 
 </style>
