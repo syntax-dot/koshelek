d72f8d5c375b6bbffc4f88c36fcabefcc0993520
Aleksandr Alekseev <alekseev.a@atbgroup.ru>
2024-05-24 10:14:04 +0300

    wip

--- src/hooks/use-order-book.ts
+++ src/hooks/use-order-book.ts
@@ -2,6 +2,8 @@ import {binanceWsInjectable} from "../external-api/binance-ws.injectable";
 import {onUnmounted, Ref, ref, watch} from "vue";
 import {Subscription} from "rxjs";
 import {Order, OrderItem} from "../types";
+import {RestMarketTypes} from "@binance/connector-typescript";
+import {binanceApiInjectable} from "../external-api/binance-api.injectable";
 
 function isOrderItem(arg: any): arg is OrderItem {
   return !!arg
@@ -10,16 +12,29 @@ function isOrderItem(arg: any): arg is OrderItem {
     && Array.isArray(arg.b)
 }
 
+const options: RestMarketTypes.orderBookOptions = {
+  limit: 100,
+};
+
+
 export function useOrderBook(symbols: Readonly<Ref<string[]>>) {
   const askOrders = ref<Order[]>([])
   const bidOrders = ref<Order[]>([])
 
   const {subscribeForSymbols} = binanceWsInjectable.inject()
+  const {client} = binanceApiInjectable.inject()
 
   let subscription: Subscription | null = null
 
-  watch(symbols, (value, oldValue) => {
-    if (subscription) subscription.unsubscribe()
+  watch(symbols, async (value, oldValue) => {
+    const formattedSymbol = value[0].split('@')[0].toUpperCase();
+
+    const binanceApiRes = await client.orderBook(formattedSymbol, options)
+
+    askOrders.value = binanceApiRes.asks.map(it => it.map(parseFloat) as Order)
+    bidOrders.value = binanceApiRes.bids.map(it => it.map(parseFloat) as Order)
+
+    if (subscription && oldValue) subscription.unsubscribe()
 
     const pair$ = subscribeForSymbols(value)
 
